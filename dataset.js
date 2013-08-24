367dd1322ac23dae05caf05e21237e166335a697
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-08-24 03:11:54 +0200

    animating the pie chart

---
+++ dataset.js
@@ -0,0 +1,93 @@
+dataset = [
+  [{
+    "label": "Treatment",
+    "value": 100
+  }, {
+    "label": "Untreated",
+    "value": 0
+  }, {
+    "label": "Dead untreated",
+    "value": 0
+  }, {
+    "label": "Dead treated",
+    "value": 0
+  }],
+  [{
+    "label": "Treatment",
+    "value": 80
+  }, {
+    "label": "Untreated",
+    "value": 20
+  }, {
+    "label": "Dead untreated",
+    "value": 0
+  }, {
+    "label": "Dead treated",
+    "value": 0
+  }],
+  [{
+    "label": "Treatment",
+    "value": 70
+  }, {
+    "label": "Untreated",
+    "value": 15
+  }, {
+    "label": "Dead untreated",
+    "value": 14
+  }, {
+    "label": "Dead treated",
+    "value": 1
+  }],
+  [{
+    "label": "Treatment",
+    "value": 60
+  }, {
+    "label": "Untreated",
+    "value": 20
+  }, {
+    "label": "Dead untreated",
+    "value": 18
+  }, {
+    "label": "Dead treated",
+    "value": 2
+  }],
+  [{
+    "label": "Treatment",
+    "value": 45
+  }, {
+    "label": "Untreated",
+    "value": 30
+  }, {
+    "label": "Dead untreated",
+    "value": 22
+  }, {
+    "label": "Dead treated",
+    "value": 3
+  }],
+  [{
+    "label": "Treatment",
+    "value": 40
+  }, {
+    "label": "Untreated",
+    "value": 25
+  }, {
+    "label": "Dead untreated",
+    "value": 31
+  }, {
+    "label": "Dead treated",
+    "value": 4
+  }],
+  [{
+    "label": "Treatment",
+    "value": 25
+  }, {
+    "label": "Untreated",
+    "value": 20
+  }, {
+    "label": "Dead untreated",
+    "value": 45
+  }, {
+    "label": "Dead treated",
+    "value": 5
+  }]
+];
